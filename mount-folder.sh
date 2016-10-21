093b8074b58c827255caef6c925f845a228c0f9f
Linh Le <dev.linhlv@gmail.com>
2016-10-21 22:34:35 +0700

    Add bin/bash

--- mount-folder.sh
+++ mount-folder.sh
@@ -1,1 +1,2 @@
-sudo mount //192.168.11.20/Software/WinRAR/Win -t cifs -o uid=1000,gid=1000,username=dev.linhlv@gmail.com /mnt/jdrive
+#!/bin/bash
+sudo mount //192.168.11.20/Software/WinRAR/Win -t cifs -o uid=1000,gid=1000,username=dev.linhlv@gmail.com,password=ThanhNha9888 /mnt/jdrive
